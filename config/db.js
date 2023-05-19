225f4ea5dc84e49f6b0053e3cb7ba927fe6eb595
nahaktarun <tarunnahak25@gmail.com>
2023-05-19 22:43:10 +0530

    ADDED - mongodb connection with mongoose

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+const connectDB = async () =>{
+
+    const conn =  await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+    });
+
+    console.log(`mongodb Connected: ${conn.connection.host}`);
+}
+
+module.exports = connectDB;
+
