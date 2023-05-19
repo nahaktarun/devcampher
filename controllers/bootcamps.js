4a13891cae931c4d73a7b7690c91720a75b28246
nahaktarun <tarunnahak25@gmail.com>
2023-05-19 21:53:28 +0530

    ADDED - bootcamps controllers

---
+++ controllers/bootcamps.js
@@ -0,0 +1,39 @@
+// @desc Get all bootcamps
+// @route  GET /api/v1/bootcamps
+// @access Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: "true", message: "show all bootcamps" });
+};
+
+// @desc Get a  single bootcamp
+// @route  GET /api/v1/bootcamps/:id
+// @access Public
+exports.getBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: "true", message: `get single bootcamp ${req.params.id}` });
+};
+
+// @desc    Create a bootcamp
+// @route  POST /api/v1/bootcamps/
+// @access private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: "true", message: "create a bootcamp" });
+};
+
+// @desc    update a bootcamp
+// @route  POST /api/v1/bootcamps/:id
+// @access private
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: "true", message: `update bootcamp ${req.params.id}` });
+};
+// @desc    delete a bootcamp
+// @route  DELETE /api/v1/bootcamps/:id
+// @access private
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: "true", message: `delete bootcamp ${req.params.id}` });
+};
