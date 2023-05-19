e13fdab12fdce8171ecde37999cd09920ff8d564
nahaktarun <tarunnahak25@gmail.com>
2023-05-19 22:07:51 +0530

    ADDED - Custom/morgan logger middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -2,7 +2,7 @@
 // @route  GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: "true", message: "show all bootcamps" });
+  res.status(200).json({ success: "true", message: "show all bootcamps"});
 };
 
 // @desc Get a  single bootcamp
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,29 +1,18 @@
 const express = require("express");
+const {
+  getBootcamp,
+  getBootcamps,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
 const router = express.Router();
 
-
-router.get("/", (req, res) => {
-  res.status(200).json({ success: "true", message: "show all bootcamps" });
-});
-router.get("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: "true", message: `get single bootcamp ${req.params.id}` });
-});
-router.post("/", (req, res) => {
-  res.status(200).json({ success: "true", message: "create a bootcamp" });
-});
-router.put("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: "true", message: `update bootcamp ${req.params.id}` });
-});
-router.delete("/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: "true", message: `delete bootcamp ${req.params.id}` });
-});
-
+router.route("/").get(getBootcamps).post(createBootcamp);
+router
+  .route("/:id")
+  .put(updateBootcamp)
+  .get(getBootcamp)
+  .delete(deleteBootcamp);
 
 module.exports = router;
-
